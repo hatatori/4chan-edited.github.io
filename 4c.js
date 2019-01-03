97195fdd3cdbeff154af30976d5fe6c2edefb53e
Hatatori Nitsukawa <kcireh.i@gmail.com>
2019-01-03 21:56:26 -0200

    Update 4c.js

--- 4c.js
+++ 4c.js
@@ -9,7 +9,7 @@ for( i=0 ; i<f.length;i++){
 }
 
 sbody = "body{background-color:black;text-align:center;}"
-simg = "img{margin:5;height:100}"
+simg = "img{margin:5;height:100;width:20vh}"
 sfora = "#fora{width: 100%;height: 100%; background-color: rgba(0,0,0,0.5);position: fixed;top: 0;left: 0;display:none}"
 svid = "#vid{max-width: 1000;max-height:90vh;position:absolute;right: 0;left: 0;bottom: 0;top: 0;margin: auto;}"
 
