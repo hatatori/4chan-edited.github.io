0b37d8d2b1928d0688330dc186a738acefe2478a
Hatatori Nitsukawa <kcireh.i@gmail.com>
2019-01-03 21:28:27 -0200

    Add files via upload

---
+++ 4c.js
@@ -0,0 +1,123 @@
+f = document.querySelectorAll(".fileThumb")
+t = ""
+
+for( i=0 ; i<f.length;i++){
+	img = f[i].querySelector("img").src
+	link = f[i].href
+
+	t += "<span val='"+link+"'><img src='"+img+"'></span>"
+}
+
+sbody = "body{background-color:black;text-align:center;}"
+simg = "img{margin:10}"
+sfora = "#fora{width: 100%;height: 100%; background-color: rgba(0,0,0,0.5);position: fixed;top: 0;left: 0;display:none}"
+svid = "#vid{max-width: 1000;max-height:90vh;position:absolute;right: 0;left: 0;bottom: 0;top: 0;margin: auto;}"
+
+t += "<div id='fora' style="+sfora+"><div id='dentro'></div></div>"
+
+document.write(t)
+
+estilo = document.createElement("style")
+estilo.innerHTML = simg+sfora+svid+sbody
+
+document.head.appendChild(estilo)
+
+window.onclick=function(e){
+	try{
+		link = e.path[1].getAttribute("val")
+		extencao = link.split(".").pop()
+
+		if(extencao == "webm"){
+			dentro.innerHTML = "<video id='vid' controls autoplay src='"+link+"'></video>"
+			showHide(fora)
+
+		}else{
+			dentro.innerHTML = "<img id='vid' src='"+link+"' >"
+			showHide(fora)
+			drag(vid)
+
+			vid.onclick=function(e){
+				e.stopPropagation()
+				e.preventDefault()
+			}
+			zoom(vid)
+		}
+
+
+	}catch(err){}
+}
+
+window.onkeyup=function(e){
+	if(e.key == "Escape"){
+		showHide(fora)
+		dentro.innerHTML = ""
+	}
+}
+
+function showHide(div){
+	if(div.style.display == "" || div.style.display == "none" ){
+		div.style.display = "block"
+		document.body.style.overflow="hidden"
+	}
+	else{
+		div.style.display = "none"
+		document.body.style.overflow="auto"
+	}
+}
+
+fora.onclick=()=>{showHide(fora)}
+
+function zoom(div){
+    k = 1
+    div.onwheel=function(e){
+
+        if(e.deltaY < 0)
+            k+=0.1
+        else
+            k-=0.1
+
+        this.style.transform = "scale("+k+")"
+
+    }
+}
+
+
+//outra parte
+
+
+function drag(div){
+
+	if(div.style.position == "")
+		div.style.position="absolute"
+
+	div.style.top = 0
+	div.style.left = 0
+
+
+	k = div['drag'] = false
+
+	div.onmousedown=function(e){
+		this['drag'] = true
+		k = this
+	}
+
+	document.onmouseup=function(e){
+		this['drag'] = false
+		k = this
+	}
+
+	document.onmousemove=function(e){
+		x = e.movementX
+		y = e.movementY
+
+		if(e.buttons > 0 && k['drag']){
+			k.style.top = parseInt(k.style.top)+y+"px"
+			k.style.left = parseInt(k.style.left)+x+"px"
+		}		
+
+		return false
+	}
+
+}
+
+
