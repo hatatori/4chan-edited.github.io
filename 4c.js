2d4ea6208a37501ebe01e44e7ee2e01f8a086dd3
Hatatori Nitsukawa <kcireh.i@gmail.com>
2019-04-28 22:30:08 -0300

    Update 4c.js

--- 4c.js
+++ 4c.js
@@ -1,98 +1,84 @@
+thread = document.getElementsByClassName("thread")
+im = []
+d2 = ""
 
+for(i=0;i<thread.length;i++){
+    im[i] = { 
+        img:thread[i].querySelector("img").src,
+        link:thread[i].querySelector("a").href
+    }
+	d2 += "<img class='cat' url='"+im[i].link+"' src='"+im[i].img+"'>"	
+}
 
+document.write("<div id='d1'></div><div id='d2'>"+d2+"</div>")
+go()
+botaoDireito()
 
-window.onload=function(){
+s_d1 = "*{background-color:black;text-align:center;outline:none}"
+s_d1 += "p {    width: 300;    height: 200;    text-align: center;    display: inline-block; margin:5}"
+s_d1 += "#d1 { width: 80%; height:100%;    position: absolute;    top: 0;    left: 0; overflow-y:scroll}"
+s_d1 += "#d1 img, #d1 video {    max-height: 100%;    max-width: 100%; }"
+s_d2 = "#d2 {    width: 20%;   height: 100%;    background-color: black;    position: absolute;    top: 0;    right: 0;    overflow-y: scroll; }"
+s_d2 += "#d2 img {    width: 50%;    max-height: 20%; } "
 
-	
+document.head.innerHTML = "<style>"+s_d1+s_d2+"</style>"
 
-	//pega tudo do catálogo
+function conteudo(url){
+    console.log(url)
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            codigo = xhttp.responseText;
+            codigo = codigo.match(/File: <a href=\"(.+?)\"/g)
+            codigo = codigo.map(e=>{return e.replace(/File: <a href=|\"/g,"")})
 
-	thread = document.getElementsByClassName("thread")
-	im = []
-	div = ""
+            codigo = codigo.map(e=>{
+                if(e.match(/webm/g))
+                    return "<p><video autoplay loop muted src='"+e+"'></video></p>"
+                else
+                    return "<p><a href='"+e+"' target=blank><img src='"+e+"'></a></p>"
+            })
+            codigo = codigo.join("")
 
-	for(i=0;i<thread.length;i++){
-		im[i] = { 
-			img:thread[i].querySelector("img").src,
-			link:thread[i].querySelector("a").href
+			d1.innerHTML = codigo
+        }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
+
+function go(){
+	ct = document.getElementsByClassName("cat")
+	for(j=0;j<ct.length;j++){
+		ct[j].onclick=function(e){
+			u = this.getAttribute('url')
+            console.log(u)
+			conteudo(u)
 		}
-		div+="<a id='tx' val="+im[i].link+"><img src='"+im[i].img+"'' width=50% max-height:100 ></a>"
 	}
-
-	estilo_geral = "padding:0;margin:0;background-color:black"
-	estilo_direito = "width:20%;height:100%;position:fixed;right:0;top:0;overflow-y:scroll"
-	estilo_esquerdo = "width:80%;height:100%;"
-	estilo_iframe = "width:100%;height:100%;border:none"
-
-	d = "<div id='esquerdo' style="+estilo_esquerdo+"><iframe id='ifr' style="+estilo_iframe+"></iframe></div><div id='direito' style="+estilo_direito+">"+div+"</div>"
-
-	document.write(d)
-
-	for(i=0;i<tx.length;i++){
-		tx[i].onclick=function(e){
-			ifr.src=this.getAttribute("val")
-
-			ativo = this
-		}
-	}
-
-	document.body.style = estilo_geral
-
-	botaoDireito()
-
-	//formata a página
-
-	ifr.onload=function(){
-
-		try{
-			fl = ifr.contentDocument.body.innerHTML.match(/File: <a href=\"(.+?)\"/g)
-			fl = fl.map(e=>{return e.replace(/File: <a href=|\"/g,"")})
-
-			fil = fl.map(e=>{
-				if(e.match(/webm/g))
-					return "<video autoplay loop muted controls src='"+e+"'></video>"
-				else
-					return "<a href='"+e+"' target=blank><img src='"+e+"'></a>"
-			})
-
-			fil = fil.join("")
-
-			ifr.contentDocument.write(fil)
-
-			st = document.createElement("style")
-			s = "*{background-color:black;text-align:center;height:300}video,img{height:200;max-width:300;margin:5}"
-			st.innerHTML=s
-			ifr.contentDocument.body.appendChild(st)
-
-			botaoDireito()
-			
-		}catch(err){}
-	}
-
 }
 
 function botaoDireito(){
-	ifr.contentWindow.oncontextmenu=function(g){
-		str = g.target.src
-		el = document.createElement('textarea');
-		el.value = str;
-		document.body.appendChild(el);
-		el.select();
-		document.execCommand('copy');
-		document.body.removeChild(el);
-		g.preventDefault()
-	}
+    window.oncontextmenu=function(g){
+        str = g.target.src
+        el = document.createElement('textarea');
+        el.value = str;
+        document.body.appendChild(el);
+        el.select();
+        document.execCommand('copy');
+        document.body.removeChild(el);
+        g.preventDefault()
+    }
 
-	ifr.contentWindow.onmouseover=function(e){
-		e.target.muted=false
-		e.target.volume=1
-	}
+    window.onmouseover=function(e){
+        e.target.muted=false
+        e.target.volume=1
+        e.target.setAttribute("controls",1)
+    }
 
-	ifr.contentWindow.onmouseout=function(e){
-		e.target.muted=true
-		e.target.volume=0
-	}
+    window.onmouseout=function(e){
+        e.target.muted=true
+        e.target.volume=0
+        e.target.removeAttribute("controls")
+    }
 }
-
-
-
